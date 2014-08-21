d47124ecc1b27c43a8c89032c21bbae2c0500422
Rohun Banerji <rohunb@gmail.com>
2014-08-21 10:06:26 -0400

    some refactoring for efficiency

--- TurnBasedTest/Assets/Scripts/TurnDelayBasedTurns/TurnBasedCombatSystem.cs
+++ TurnBasedTest/Assets/Scripts/TurnDelayBasedTurns/TurnBasedCombatSystem.cs
@@ -33,45 +33,57 @@ public class TurnBasedCombatSystem : MonoBehaviour
 
     IEnumerator StartCombat()
     {
-        int entityCount = entities.Count;
+        int entityCount = entities.Count; //caching list count
+
+        TurnBasedEntity firstEntity; //will contain the first unit to move every round
+
         while (combatPhaseOn && entityCount > 0)
         {
-            //sorting according to time left to turn - first in the list has the lowest time remaining until it's turn
-            entities = entities.OrderBy(entity => entity.TimeLeftToTurn).ToList<TurnBasedEntity>();
             
-            //currentTime gets set to the lowest Time To Turn
-            currentTime=entities[0].TimeLeftToTurn;
-            //the first unit in the list takes it's turn
-            yield return StartCoroutine(entities[0].StartTurn());
-            turnHistory.Add(entities[0]);
+            //gets the unit with the least TimeLeftToTurn
+            firstEntity = entities.Aggregate((current, next) =>
+                                        current.TimeLeftToTurn < next.TimeLeftToTurn
+                                        ? current : next);
+
+
+            //records the current unit's time left to turn, to subtract from all units
+            currentTime = firstEntity.TimeLeftToTurn;
+
+            //the first unit takes it's turn
+            yield return StartCoroutine(firstEntity.StartTurn());
+
+            //adds to the turn history - mainly for GUI purposes
+            turnHistory.Add(firstEntity);
 
             
-            while(!Input.GetKeyDown(KeyCode.Return))
-            {
-                yield return null;
-            }
+            //while(!Input.GetKeyDown(KeyCode.Return))
+            //{
+            //    yield return null;
+            //}
                        
-
+            //subtracts the currentTime from all units' timers
             for (int i = 0; i < entityCount; i++)
             {
                 entities[i].TimeLeftToTurn -= currentTime;
             }
 
+            //re-caching list size, in case any unit got removed
             entityCount = entities.Count;
         }
     }
 
     void OnGUI()
     {
-        GUILayout.BeginArea(new Rect(20, 20, 200, 100));
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("<size=24>Turn Order: </size>");
-        for (int i = 0; i < entities.Count; i++)
-        {
-            GUILayout.Label( "<size=24> " + entities[i].name + "</size>");
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.EndArea();
+        //GUILayout.BeginArea(new Rect(20, 20, 1000, 500));
+        //GUILayout.BeginHorizontal();
+        //GUILayout.Label("<size=24>Current Time: "+currentTime+" </size>");
+        //GUILayout.Label("<size=24>Turn Order: </size>");
+        //for (int i = 0; i < entities.Count; i++)
+        //{
+        //    GUILayout.Label("<size=24> " + entities[i].name + "</size>");
+        //}
+        //GUILayout.EndHorizontal();
+        //GUILayout.EndArea();
 
         GUILayout.BeginArea(new Rect(Screen.width - 300, 50, 200, Screen.height - 50));
         GUILayout.BeginVertical();
