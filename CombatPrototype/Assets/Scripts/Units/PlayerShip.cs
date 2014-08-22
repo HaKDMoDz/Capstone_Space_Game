6d4ab882f3b03a3db148efa230fa5e6edaf969bc
Rohun Banerji <rohunb@gmail.com>
2014-08-22 11:28:20 -0400

    combat prototype updates

--- CombatPrototype/Assets/Scripts/Units/PlayerShip.cs
+++ CombatPrototype/Assets/Scripts/Units/PlayerShip.cs
@@ -4,6 +4,19 @@ using System.Collections;
 public class PlayerShip : TurnBasedUnit 
 {
 
+    public int groundLayer;
+
+
+    //cached components
+    ShipMove shipMove;
+
+    public override void Awake()
+    {
+        base.Awake();
+        shipMove = GetComponent<ShipMove>();
+    }
+
+
     public override IEnumerator ExecuteTurn()
     {
         Debug.Log(unitName + " (PlayerShip) starts turn");
@@ -11,12 +24,35 @@ public class PlayerShip : TurnBasedUnit
 
         yield return base.ExecuteTurn();
 
+        //ends the player's turn only upon hitting Space
         while(!Input.GetKeyDown(KeyCode.Space))
         {
+        
+            if(Input.GetMouseButtonDown(1))
+            {
+                shipMove.Move();
+            }
+            
             yield return null;
         }
 
         projector.enabled = false;
         Debug.Log(unitName + " (PlayerShip) ends turn");
     }
+
+    Vector3 GetWorldCoordsFromMouse(Vector3 mousePos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, 0f));
+        RaycastHit hit;
+
+        Vector3 worldCoords = _trans.position;
+
+        if (Physics.Raycast(ray, out hit, 1000f, 1 << groundLayer))
+        {
+            worldCoords = hit.point;
+            worldCoords.y = _trans.position.y;
+        }
+        return worldCoords;
+    }
+
 }
---
+++ CombatPrototype/Assets/ShipMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+
+public class ShipMove : MonoBehaviour 
+{
+
+    public float moveSpeed;
+    public float turnSpeed;
+
+    [SerializeField]
+    float movementEpsilon;
+
+    //cached components
+    Transform trans;
+
+    void Awake()
+    {
+        trans = transform;
+    }
+
+    public IEnumerator Move(Vector3 destination)
+    {
+
+        Vector3 moveDir = destination - trans.position;
+
+        while(Vector3.SqrMagnitude(moveDir) > movementEpsilon*movementEpsilon)
+        {
+            trans.Translate(moveDir * moveSpeed * Time.deltaTime);
+            moveDir = destination - transform.position;
+            yield return null;
+
+        }
+
+    }
+
+
+}
