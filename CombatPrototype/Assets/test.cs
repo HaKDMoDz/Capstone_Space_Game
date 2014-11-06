9e85eef0a161fa6bf7bf8a21ee9fd6ff4c9d52e5
rohunb <rohunb@gmail.com>
2014-11-06 17:01:38 -0500

    added fighter model

---
+++ CombatPrototype/Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class test : MonoBehaviour 
+{
+    void Start()
+    {
+        Invoke("Anim", 2.0f);
+    }
+    void Anim()
+    {
+        GetComponent<Animation>().Play("Take 001");
+        
+    }
+
+}
