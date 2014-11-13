1beb9b3bf55e28426f06adf6f187bd8d52d6064c
Rohun Banerji <rohunb@gmail.com>
2014-11-12 22:13:02 -0500

    added SceneSelector Menu Item in MainProject

---
+++ MainProject/Assets/Editor/SceneSelector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneSelector : Editor 
+{
+    const string menuName = "Open Scene";
+
+    [MenuItem(menuName + "/Ship Design Scene")]
+    public static void OpenShipDesignScene()
+    {
+        OpenScene("ShipDesignScene");
+    }
+
+    [MenuItem(menuName + "/Test Scene")]
+    public static void OpenTestScene()
+    {
+        OpenScene("TestScene");
+    }
+
+    static void OpenScene(string name)
+    {
+        if(EditorApplication.SaveCurrentSceneIfUserWantsTo())
+        {
+            EditorApplication.OpenScene("Assets/Scenes/" + name + ".unity");
+        }
+    }
+}
