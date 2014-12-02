5dcaa298ca913d3b5740741544e7f4c430e274da
Rohun Banerji <rohunb@gmail.com>
2014-12-01 23:35:11 -0500

    refactoring ObjectPool - incomplete

--- MainProject/Assets/Scripts/Framework/ObjectPool.cs
+++ MainProject/Assets/Scripts/Framework/ObjectPool.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 #region AdditionalData
 [Serializable]
@@ -19,13 +20,12 @@ public class ObjectPool : Singleton<ObjectPool>
 
     #region EditorExposed
     [SerializeField]
-    private List<ObjectPoolEntry> PrefabList; //the object prefabs that the object pool will create from
-    [SerializeField]
-    private int defaultBufferAmount = 5;
+    private List<ObjectPoolEntry> prefabList; //the object prefabs that the object pool will create from
     #endregion //EditorExposed
 
     #region InternalFields
-    private List<GameObject> pooledObjects;
+    //private List<GameObject> pooledObjects;
+    private Dictionary<GameObject, Queue<GameObject>> pooledObjectTable;
     private Transform container; //an empty to parent pooled objects under
     #endregion //InternalFields
 
@@ -34,91 +34,91 @@ public class ObjectPool : Singleton<ObjectPool>
     #region Methods
 
     #region Public
+    /// <summary>
+    /// Returns a new GameObject based on the name (objectType) provided. Will return null if object is not found.
+    /// </summary>
+    /// <param name="objectName">Name of the game object to instantiate</param>
+    /// <param name="onlyPooled">
+    /// If true, will only return a pooled object, if false, will instantiate a new object if out of pooled objects
+    /// </param>
+    public GameObject GetPooledObject(string objectName, bool onlyPooled)
+    {
+        //for (int i = 0; i < objectPrefabs.Length; i++)
+        //{
+        //    if (objectPrefabs[i].name == objectName)
+        //    {
+        //        if (pooledObjects[i].Count > 0)
+        //        {
+        //            GameObject pooledObject = pooledObjects[i][0];
+        //            pooledObjects[i].RemoveAt(0);
+        //            pooledObject.transform.parent = null;
+        //            pooledObject.SetActive(true);
+        //            return pooledObject;
+        //        }
+        //        else if (!onlyPooled)
+        //        {
+        //            Debug.LogError("Out of Pooled objects - instantiating new object");
+        //            return Instantiate(objectPrefabs[i]) as GameObject;
+        //        }
+        //        break;
+        //    }
+        //}
 
+#if !NO_DEBUG
+        Debug.LogError("Object not found or out of pooled objects");
+#endif
+        return null; //object was not found
+    }
+
+    public void PoolObject(GameObject obj)
+    {
+        //for (int i = 0; i < objectPrefabs.Length; i++)
+        //{
+        //    if (objectPrefabs[i].name == obj.name)
+        //    {
+        //        obj.SetActive(false);
+        //        obj.transform.position = container.position;
+        //        obj.transform.parent = container;
+        //        pooledObjects[i].Add(obj);
+        //        return;
+        //    }
+        //}
+    }
     #endregion //Public Methods
 
     #region Private
-    //private void Start()
-    //{
-    //    container = new GameObject("ObjectPool").transform;
+    private void Start()
+    {
 
-    //    pooledObjects = new List<GameObject>(PrefabList.Count);
+#if UNITY_EDITOR
 
-    //    //create a list for each object prefab
-    //    for (int i = 0; i < PrefabList.Count; i++)
-    //    {
-    //        pooledObjects[i] = new List<GameObject>();
+        for (int i = 0; i < prefabList.Count; i++)
+        {
+            int objNameCount = prefabList.Where(p => p.gameObject.name == prefabList[i].gameObject.name).Count();
+            if(objNameCount > 1)
+            {
+                Debug.LogError("Duplicate prefab names found", prefabList[i].gameObject);
+                return;
+            }
+        }
+#endif
+        
+        container = new GameObject("ObjectPool").transform;
+        pooledObjectTable = new Dictionary<GameObject, Queue<GameObject>>();
 
-    //        int bufferAmount = i < amountToBuffer.Length ? amountToBuffer[i] : defaultBufferAmount;
+        //create a list for each object prefab
+        for (int i = 0; i < prefabList.Count; i++)
+        {
+            pooledObjectTable.Add(prefabList[i].gameObject, new Queue<GameObject>());
 
-    //        for (int j = 0; j < bufferAmount; j++)
-    //        {
-    //            GameObject newObj = Instantiate(objectPrefabs[i]) as GameObject;
-    //            newObj.name = objectPrefabs[i].name;
-    //            PoolObject(newObj);
-    //        }
-    //    }
-    //}
+            for (int j = 0; j < prefabList[i].amountToBuffer; j++)
+            {
+                GameObject newObj = Instantiate(prefabList[i].gameObject) as GameObject;
+                newObj.name = prefabList[i].gameObject.name;
+                PoolObject(newObj);
+            }
+        }
+    }
     #endregion //private methods
     #endregion //Methods
-
-    
-
-    //#endregion //Private Methods
-
-    //#endregion //Methods
-    
-    
-
-    ///// <summary>
-    ///// Returns a new GameObject based on the name (objectType) provided. Will return null if object is not found.
-    ///// </summary>
-    ///// <param name="objectName">Name of the game object to instantiate</param>
-    ///// <param name="onlyPooled">
-    ///// If true, will only return a pooled object, if false, will instantiate a new object if out of pooled objects
-    ///// </param>
-    //public GameObject GetPooledObject(string objectName, bool onlyPooled)
-    //{
-    //    for (int i = 0; i < objectPrefabs.Length; i++)
-    //    {
-    //        if(objectPrefabs[i].name == objectName)
-    //        {
-    //            if(pooledObjects[i].Count>0)
-    //            {
-    //                GameObject pooledObject = pooledObjects[i][0];
-    //                pooledObjects[i].RemoveAt(0);
-    //                pooledObject.transform.parent = null;
-    //                pooledObject.SetActive(true);
-    //                return pooledObject;
-    //            }
-    //            else if(!onlyPooled)
-    //            {
-    //                Debug.LogError("Out of Pooled objects - instantiating new object");
-    //                return Instantiate(objectPrefabs[i]) as GameObject;
-    //            }
-    //            break;
-    //        }
-    //    }
-    //    Debug.LogError("Object not found or out of pooled objects");
-    //    return null; //object was not found
-    //}
-
-    //public void PoolObject(GameObject obj)
-    //{
-    //    for (int i = 0; i < objectPrefabs.Length; i++)
-    //    {
-    //        if(objectPrefabs[i].name ==obj.name)
-    //        {
-    //            obj.SetActive(false);
-    //            obj.transform.position = container.position;
-    //            obj.transform.parent = container;
-    //            pooledObjects[i].Add(obj);
-    //            return;
-    //        }
-    //    }
-    //}
-
-
-
-
 }
