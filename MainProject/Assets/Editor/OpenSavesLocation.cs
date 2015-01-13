439c5f3a27b2abc0ea519f6c7a805de10615c35e
Rohun Banerji <rohunb@gmail.com>
2015-01-12 23:02:15 -0500

    added Editor Script to open Save Game directory in windows explorer

---
+++ MainProject/Assets/Editor/OpenSavesLocation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class OpenSavesLocation  
+{
+    [MenuItem("Custom/Open Saves Location")]
+    static void OpenSaveLocation()
+    {
+        string path = Application.persistentDataPath.Replace("/", "\\");
+        System.Diagnostics.Process.Start("explorer.exe", "/select," + path);
+    }
+	
+}
