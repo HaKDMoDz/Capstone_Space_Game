33bdf959b4136c882ca18f9274296bb3e1c12e88
Rohun Banerji <rohunb@gmail.com>
2015-01-15 22:22:42 -0500

    added Editor script to delete all save files from windows

--- MainProject/Assets/Editor/OpenSavesLocation.cs
+++ MainProject/Assets/Editor/OpenSavesLocation.cs
@@ -6,11 +6,16 @@ using System.IO;
 
 public class OpenSavesLocation  
 {
-    [MenuItem("Custom/Open Saves Location")]
+    [MenuItem("Custom/Saves/Open Saves Location")]
     static void OpenSaveLocation()
     {
         string path = Application.persistentDataPath.Replace("/", "\\");
         System.Diagnostics.Process.Start("explorer.exe", "/select," + path);
     }
-	
+    [MenuItem("Custom/Saves/Delete All Saves")]
+    static void DeleteAllSaves()
+    {
+        Debug.LogWarning("Deleting all saves");
+        Directory.Delete(Application.persistentDataPath, true);
+    }
 }
