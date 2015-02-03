1b919ad5b77faf7ac02f8b4a2b94095f34201641
Alexander McCann <drenzior@gmail.com>
2015-02-02 23:20:35 -0500

    GalaxyMap GUI started.
    next  step: setup buttons to scripts on Planet_Mission

--- MainProject/Assets/Scripts/Environment/SystemObject.cs
+++ MainProject/Assets/Scripts/Environment/SystemObject.cs
@@ -4,9 +4,5 @@ using System.Collections.Generic;
 
 public class SystemObject : MonoBehaviour 
 {
-    private void OnMouseDown()
-    {
-        Debug.Log("clicked");
-        GameObject.Find("Mothership").GetComponent<Mothership>().Orbiting = true;
-    }
+
 }
---
+++ MainProject/Assets/Scripts/GUI/Planet_Mission.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet_Mission : MonoBehaviour 
+{
+
+    public SystemObject destination;
+    public GameObject[] targets;
+    [SerializeField]
+    private bool completed;
+    public bool Completed
+    {
+        get { return completed; }
+        set { completed = value; }
+    }
+    [SerializeField]
+    private int creditReward;
+    public int CreditReward
+    {
+        get { return creditReward; }
+        set { creditReward = value; }
+    }
+
+	void Awake () 
+    {
+
+	}
+	
+}
