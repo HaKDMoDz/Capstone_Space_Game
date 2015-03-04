4c39f117d71635649c8e9c4a142fcbd5557d31dd
Alexander McCann <drenzior@gmail.com>
2015-03-04 16:01:24 -0500

    Working on invasion UI

---
+++ MainProject/Assets/Scripts/GUI/InvasionGUIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvasionGUIManager : MonoBehaviour 
+{
+    [SerializeField]
+    public FillBar fillbar;
+
+	void Start () 
+    {
+        fillbar.SetValue(0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
