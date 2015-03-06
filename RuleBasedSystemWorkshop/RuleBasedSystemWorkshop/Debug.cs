f29fbed1c25f101c9023095b5e3c933d5444faf2
Alexander McCann <drenzior@gmail.com>
2015-03-06 12:07:02 -0500

    Started work on rule based AI system
    Robson will be so happy -.-

---
+++ RuleBasedSystemWorkshop/RuleBasedSystemWorkshop/Debug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Debug
+{
+    public static void Log(string _text)
+    {
+        PrintError("Log: " + _text);
+    }
+
+    public static void LogWarning(string _text)
+    {
+        PrintError("Warning: " + _text);
+    }
+
+    public static void LogError(string _text)
+    {
+        PrintError("Error: " + _text);
+    }
+
+    private static void PrintError(string _text)
+    {
+        Console.WriteLine(_text);
+    }
+}
---
+++ RuleBasedSystemWorkshop/RuleBasedSystemWorkshop/WorkingMemory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// This is to handle Unity Vector3s. DELETE during import to Unity
+/// </summary>
+public class Vector3
+{
+    private float x, y, z;
+
+    public static Vector3 zero = new Vector3(0, 0, 0);
+
+    Vector3(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+}
+
+class WorkingMemory
+{
+    List<Vector3> enemyPositions;
+    List<Vector3> playerPositions;
+
+    public WorkingMemory()
+    {
+        Debug.Log("Working Memory Created");
+
+        enemyPositions = new List<Vector3>();
+        playerPositions = new List<Vector3>();
+    }
+
+    /// <summary>
+    /// This function will collect "facts" from the units in combat for use by the Rule Interpreter
+    /// </summary>
+    public void PopulateWorkingMemory()
+    {
+        Vector3 someVector = Vector3.zero;
+    }
+}
