22d7e1964a39ca564f24e07cf89238471bea0123
Rohun Banerji <rohunb@gmail.com>
2015-03-09 21:58:48 -0400

    added railgun effect

--- MainProject/Assets/Scripts/ShipComponents/Comp_Wpn_Railgun.cs
+++ MainProject/Assets/Scripts/ShipComponents/Comp_Wpn_Railgun.cs
@@ -6,29 +6,37 @@ using UnityEngine;
 
 public class Comp_Wpn_Railgun : Component_Weapon
 {
-    [SerializeField]
-    private Color railEffectStartColour = Color.white;
-    [SerializeField]
-    private float railEffectRadius = 1.5f;
-    [SerializeField]
-    private float railEffectDuration = 0.2f;
+    //[SerializeField]
+    //private Color railEffectStartColour = Color.white;
+    //[SerializeField]
+    //private float railEffectRadius = 1.5f;
+    //[SerializeField]
+    //private float railEffectDuration = 0.2f;
+    //private LineRenderer line;
+    //int length;
 
-    private LineRenderer line;
-    int length;
+    [SerializeField]
+    private Projectile_Missile projectilePrefab;
+    [SerializeField]
+    private GameObject muzzleFlash;
+    [SerializeField]
+    private GameObject impactEffect;
+    [SerializeField]
+    private float projectileSpeed = 10.0f;
 
     public override void Init(TurnBasedUnit _parentShip)
     {
         base.Init(_parentShip);
 
-        line = GetComponentInChildren<LineRenderer>();
-        #if FULL_DEBUG
-        if(!line)
-        {
-            Debug.LogError("No LineRenderer found");
-        }
-        #endif
+        //line = GetComponentInChildren<LineRenderer>();
+        //#if FULL_DEBUG
+        //if(!line)
+        //{
+        //    Debug.LogError("No LineRenderer found");
+        //}
+        //#endif
 
-        line.enabled = false;
+        //line.enabled = false;
     }
     public override IEnumerator Fire(ShipComponent targetComp, Action OnActivationComplete)
     {
@@ -40,42 +48,67 @@ public class Comp_Wpn_Railgun : Component_Weapon
 
             targetTrans = targetComp.transform;
             shootPoint.LookAt(targetTrans);
-            length = Mathf.RoundToInt(Vector3.Distance(targetTrans.position, shootPoint.position));
-            yield return StartCoroutine(CreateRailEffect());
+            //length = Mathf.RoundToInt(Vector3.Distance(targetTrans.position, shootPoint.position));
+            //yield return StartCoroutine(CreateRailEffect());
+            //yield return StartCoroutine(DoDamage(targetComp));
+            Projectile_Missile bulletClone = (Projectile_Missile)Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
+            //bulletClone.rigidbody.AddForce(shootPoint.forward * shootImpulse, ForceMode.Impulse);
+            bulletClone.rigidbody.velocity = shootPoint.forward * projectileSpeed;
+            //muzzle flash
+            bool bulletCollided = false;
+            bulletClone.OnCollision +=
+                (GameObject other) =>
+                {
+                    if ((targetComp.ParentShip.ShieldStrength > 0.0f
+                    && other.layer == TagsAndLayers.ShipShieldLayer
+                    && other.GetComponentInParent<TurnBasedUnit>() == targetComp.ParentShip)
+                    || (other.layer == TagsAndLayers.ComponentsLayer
+                    && other.GetComponent<ShipComponent>() == targetComp))
+                    {
+                        bulletCollided = true;
+                        //explosion
+                        Destroy(bulletClone.gameObject);
+                    }
+                };
+            while(!bulletCollided)
+            {
+                yield return null;
+            }
             yield return StartCoroutine(DoDamage(targetComp));
         }
+
         OnActivationComplete();
     }
 
-    private IEnumerator CreateRailEffect()
-    {
-        line.enabled = true;
-        int factor = 2;
-        line.SetVertexCount(length*factor);
-        //Vector3 targetDir = (targetTrans.position - shootPoint.position).normalized;
+    //private IEnumerator CreateRailEffect()
+    //{
+    //    line.enabled = true;
+    //    int factor = 2;
+    //    line.SetVertexCount(length*factor);
+    //    //Vector3 targetDir = (targetTrans.position - shootPoint.position).normalized;
 
-        for (int i = 0; i < length*factor; i++)
-        {
-            Vector3 newPos = shootPoint.position;
-            float offsetX = railEffectRadius * Mathf.Cos(i + Time.time);
-            float offsetY = railEffectRadius * Mathf.Sin(i + Time.time);
+    //    for (int i = 0; i < length*factor; i++)
+    //    {
+    //        Vector3 newPos = shootPoint.position;
+    //        float offsetX = railEffectRadius * Mathf.Cos(i + Time.time);
+    //        float offsetY = railEffectRadius * Mathf.Sin(i + Time.time);
 
-            newPos += shootPoint.right * offsetX;
-            newPos += shootPoint.up * offsetY;
-            newPos += shootPoint.forward * (float)i / factor;
+    //        newPos += shootPoint.right * offsetX;
+    //        newPos += shootPoint.up * offsetY;
+    //        newPos += shootPoint.forward * (float)i / factor;
 
-            line.SetPosition(i, newPos);
-        }
-        Color currentColour = railEffectStartColour;
-        Color targetColour = currentColour.WithAplha(0.0f);
-        float currentTime = 0.0f;
-        while(currentTime <= railEffectDuration)
-        {
-            line.SetColors(currentColour, currentColour);
-            currentColour = Color.Lerp(currentColour, targetColour, currentTime);
-            currentTime += Time.deltaTime;
-            yield return null;
-        }
-        line.enabled = false;
-    }
+    //        line.SetPosition(i, newPos);
+    //    }
+    //    Color currentColour = railEffectStartColour;
+    //    Color targetColour = currentColour.WithAplha(0.0f);
+    //    float currentTime = 0.0f;
+    //    while(currentTime <= railEffectDuration)
+    //    {
+    //        line.SetColors(currentColour, currentColour);
+    //        currentColour = Color.Lerp(currentColour, targetColour, currentTime);
+    //        currentTime += Time.deltaTime;
+    //        yield return null;
+    //    }
+    //    line.enabled = false;
+    //}
 }
