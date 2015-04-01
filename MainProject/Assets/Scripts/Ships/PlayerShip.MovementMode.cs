0662acbf951433e455117ee56b28ff14e1e299e6
Alexander McCann <drenzior@gmail.com>
2015-04-01 15:41:13 -0400

    trans null fix

--- MainProject/Assets/Scripts/Ships/PlayerShip.MovementMode.cs
+++ MainProject/Assets/Scripts/Ships/PlayerShip.MovementMode.cs
@@ -106,6 +106,7 @@ public partial class PlayerShip : TurnBasedUnit
     }
     private void MouseOverSpaceGround()
     {
+        if (!trans) return;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, GlobalVars.RayCastRange, 1 << TagsAndLayers.SpaceGroundLayer))
